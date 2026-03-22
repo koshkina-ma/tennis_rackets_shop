3c2e234035885f9a2c0c2c456529e474c6440365
Maria Koshkina <54333993+koshkina-ma@users.noreply.github.com>
2026-03-23 00:36:32 +0500

    feat: favorite button ui

--- src/components/favorite-button/favorite-button.tsx
+++ src/components/favorite-button/favorite-button.tsx
@@ -1,46 +1,14 @@
 "use client";
 
-import { UserContext } from "@/providers/user";
-import { FC, use, useTransition } from "react";
-import { Link } from "../link/link";
-import { BASE_API_URL } from "@/constants/api";
-import classNames from "classnames";
+import { UserContext } from "@/app/providers/user-provider";
+import { use } from "react";
 
-const handleLogout = async () => {
-  await fetch(`${BASE_API_URL}/auth/logout`, {
-    credentials: "include",
-    method: "DELETE",
-  });
+export const ToggleFavoriteButton = () => {
+  const { user } = use(UserContext);
 
-  location.assign("/");
-};
+  if (user === undefined) {
+    return null;
+  }
 
-import styles from "./user-section.module.css";
-
-interface Props {
-  className: string;
-}
-
-export const UserSection: FC<Props> = ({ className }) => {
-  const { isAuthorized } = use(UserContext);
-
-  const [isPending, startTransition] = useTransition();
-
-  return (
-    <div className={classNames(className, styles.root)}>
-      {isAuthorized ? (
-        <button
-          disabled={isPending}
-          onClick={() => startTransition(handleLogout)}
-        >
-          Logout
-        </button>
-      ) : (
-        <>
-          <Link href='/sign-in'>Sign In</Link>
-          <Link href='/sign-up'>Sign Up</Link>
-        </>
-      )}
-    </div>
-  );
+  return <button type="button">Добавить в избранное</button>;
 };
